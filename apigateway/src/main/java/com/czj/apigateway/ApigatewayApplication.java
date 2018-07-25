aff29d545be47bcb857aa6c891a22b04d0db4dc1
czonesoft <1974064869@qq.com>
2018-07-26 04:54:32 +0800

    gateway完成

--- apigateway/src/main/java/com/czj/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/czj/apigateway/ApigatewayApplication.java
@@ -3,29 +3,11 @@ package com.czj.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-//@EnableEurekaClient
 @EnableDiscoveryClient
 public class ApigatewayApplication {
-//    @Bean
-//    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-//        return builder.routes().build();
-//    }
 
-    @Bean
-    public RouteLocator routes(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(r -> r.path("/u/**")
-                        .filters(f -> f.rewritePath("/u/(?<path>.*)", "/${path}"))
-                        .uri("lb://PROVIDERUSER")// http://localhost:8001/user/1
-                        .id("user-service"))
-                .build();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ApigatewayApplication.class, args);
